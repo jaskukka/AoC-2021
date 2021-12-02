64963561cc8241347dc36a92a689eed3f32ccc7a
jaskukka <oskari.kansanen@tuni.fi>
2021-12-02 21:56:11 +0200

    finish day 2

---
+++ Day 2/AdvMovement.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.*;
+
+public class AdvMovement {
+    public static ArrayList<String> readFile() {
+        ArrayList<String> inputList = new ArrayList<String>();
+        File file = new File("input.txt");
+        Scanner scanner = null;
+        
+        try {
+            scanner = new Scanner(file);
+        
+            while (scanner.hasNextLine()) {
+                inputList.add(scanner.nextLine());
+            }
+        }
+        catch (Exception e) {
+            if (scanner != null) {
+                scanner.close();
+            }
+        }
+        scanner.close();
+        return inputList;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<String> input = readFile();
+        
+        int horizontal = 0;
+        int vertical = 0;
+        int aim = 0;
+        String[] splitInput;
+
+        for (int i = 0; i < input.size(); i++) {
+            splitInput = input.get(i).split(" ");
+            if (splitInput[0].equals("forward")) {
+                horizontal += Integer.parseInt(splitInput[1]);
+                vertical += aim * Integer.parseInt(splitInput[1]);
+            }
+            else if (splitInput[0].equals("down")) {
+                aim += Integer.parseInt(splitInput[1]);
+            }
+            else if (splitInput[0].equals("up")) {
+                aim -= Integer.parseInt(splitInput[1]);
+            }
+        }
+
+        System.out.println("horizontal: " + horizontal);
+        System.out.println("depth: " + vertical);
+        System.out.println(horizontal*vertical);
+    }
+}

86bc85b4d2cbc8d8c857e5aa7ffe82cc1640509a
jaskukka <oskari.kansanen@tuni.fi>
2021-12-02 21:35:50 +0200

    finish day 1 part 1

--- Day 2/Movement.java
+++ Day 2/Movement.java
@@ -26,6 +26,26 @@ public class Movement {
 
     public static void main(String[] args) {
         ArrayList<String> input = readFile();
-        System.out.println(input);
+        
+        int horizontal = 0;
+        int vertical = 0;
+        String[] splitInput;
+
+        for (int i = 0; i < input.size(); i++) {
+            splitInput = input.get(i).split(" ");
+            if (splitInput[0].equals("forward")) {
+                horizontal += Integer.parseInt(splitInput[1]);
+            }
+            else if (splitInput[0].equals("down")) {
+                vertical += Integer.parseInt(splitInput[1]);
+            }
+            else if (splitInput[0].equals("up")) {
+                vertical -= Integer.parseInt(splitInput[1]);
+            }
+        }
+
+        System.out.println("horizontal: " + horizontal);
+        System.out.println("depth: " + vertical);
+        System.out.println(horizontal*vertical);
     }
 }
